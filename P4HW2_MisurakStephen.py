2307c7ad50b8b65bc9d6503b82828e2a0f17a140
Steph17738 <98050188+Steph17738@users.noreply.github.com>
2022-04-05 20:27:14 -0400

    Add files via upload

---
+++ P4HW2_MisurakStephen.py
@@ -0,0 +1,56 @@
+# CTI-110
+# P4HW2 - Pizza Order
+# Stephen Misurak 
+# 3/25/2022
+from ast import Break
+import math
+from time import process_time_ns
+from traceback import print_tb
+
+#Ask user to enter number of students she/he would like to order pizza for.  
+#Ask user for number of people that will be sharing each pizza ( 1.5, 2 or 3). 
+
+def main():
+    students = int(input('How many students do you want to order pizza for?\n'))
+    num_people = float(input('Enter number of people per pizza ( 1.5, 2 or 3):\n'))
+    #numbers allowed, input until one number is chosen
+    numbers_allowed = [1.5, 2, 3] 
+    while num_people not in numbers_allowed:
+        print('INVALID ENTRY!!!')
+        print('You should have entered 1.5, 2 or 3\n')
+        num_people = float(input('Enter number of people per pizza again. ( 1.5, 2 or 3): '))
+    #calculations for entries 
+    #If user enters 1.5 ( meaning 1.5 people per pizza), the program is to do the math and calculate number of Whole pizzas user needs to buy.
+    #If user enters 2 (2 people per pizza), the program is to calculate number of Whole pizzas user needs to buy
+    #If user enters 3 (meaning 3 people per pizza ), the program is to calculate number of Whole pizzas user needs to buy
+    if num_people == 1.5:
+        pizza = math.ceil(students / num_people)
+        price = pizza * 5
+        final_price = price * 1.06
+    elif num_people == 2:
+        pizza = math.ceil(students / num_people)
+        price = pizza * 5
+        final_price = price * 1.06
+    elif num_people == 3:
+        pizza = math.ceil(students / num_people)
+        price = pizza * 5
+        final_price = price * 1.06
+
+    #Order if inputs are correct
+    if num_people in numbers_allowed:
+        print('----Pizza Order---------')
+        print(f'Number of students {students}')
+        print(f'Pizzas Needed: {pizza}')
+        print(f'Price: ${final_price:.2f}')
+        print('-------------------------\n')
+
+    #ask if user wants to run the program again
+    run_again = str(input('Would you like to run the program again? (y for yes): '))
+    while True:
+        if run_again == 'y':
+                main()         
+        else:
+            print('Goodbye')
+            break
+                    
+main()
